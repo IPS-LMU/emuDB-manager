2e3844f728b9223e16eb702f7289830b7c328670
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2016-07-25 19:05:23 +0200

    minimal router changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,9 @@ import {DatabaseDashboardComponent} from "./database-dashboard/database-dashboar
 import {DashboardComponent} from "./dashboard/dashboard.component";
 
 const routes: RouterConfig = [
+  { path: 'overview', component: DashboardComponent},
   { path: 'databases', component: DatabaseDashboardComponent },
+  { path: '', redirectTo: '/overview', pathMatch: 'full'},
   { path: '**', component: DashboardComponent }
 ];
 
