6d30c138776d88198af078a7e9d2d7012aaaf1ec
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2016-07-28 11:12:29 +0200

    added project (sub-)routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { provideRouter, RouterConfig } from '@angular/router';
-import {DatabaseDashboardComponent} from "./database-dashboard/database-dashboard.component";
-import {DashboardComponent} from "./dashboard/dashboard.component";
+import {projectRoutes} from "./project/project.routes";
 import {ProjectComponent} from "./project/project.component";
 
+
 const routes: RouterConfig = [
-  { path: 'project', component: ProjectComponent},
-  { path: 'overview', component: DashboardComponent},
-  { path: 'databases', component: DatabaseDashboardComponent },
-  { path: '', redirectTo: '/overview', pathMatch: 'full'},
-  { path: '**', component: DashboardComponent }
+  ...projectRoutes,
+  { path: '**', component: ProjectComponent }
 ];
 
 export const appRouterProviders = [
---
+++ src/app/project/project.routes.ts
@@ -0,0 +1,15 @@
+import {RouterConfig} from '@angular/router';
+import {DatabaseDashboardComponent} from "../database-dashboard/database-dashboard.component";
+import {DashboardComponent} from "../dashboard/dashboard.component";
+import {ProjectComponent} from "./project.component";
+
+export const projectRoutes:RouterConfig = [{
+  path: 'project',
+  component: ProjectComponent,
+  children: [
+    {path: '', component: DashboardComponent},
+    {path: 'databases', component: DatabaseDashboardComponent},
+    //{ path: '', redirectTo: '/overview', pathMatch: 'full'},
+    //{path: '**', component: DashboardComponent}
+  ]
+}];
