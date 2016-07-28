8dce74cb4d45cdf39c2d6e94c87d8640e96b6951
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2016-07-28 17:14:29 +0200

    made design more responsive; added custom bootstrap

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -16,9 +16,7 @@ module.exports = function(defaults) {
       'reflect-metadata/**/*.+(ts|js|js.map)',
       'rxjs/**/*.+(js|js.map)',
       '@angular/**/*.+(js|js.map)',
-      'bootstrap/dist/**',
-      'jquery/dist/*',
-      'assets/*.png'
+      'jquery/dist/*'
     ]
   });
 };
