373e751cf1933954c1c84641fc19c71fcb038580
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2016-08-03 10:40:26 +0200

    activated new angular-forms and made login screen a proper form

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
-import {bootstrap} from '@angular/platform-browser-dynamic';
-import {enableProdMode} from '@angular/core';
-import {AppComponent, environment} from './app/';
+import {bootstrap} from "@angular/platform-browser-dynamic";
+import {enableProdMode} from "@angular/core";
+import {AppComponent, environment} from "./app/";
 import {appRouterProviders} from "./app/app.routes";
+import {disableDeprecatedForms, provideForms} from "@angular/forms";
 
 if (environment.production) {
 	enableProdMode();
 }
 
 bootstrap(AppComponent, [
-	appRouterProviders
+	appRouterProviders,
+	disableDeprecatedForms(),
+	provideForms()
 ])
 	.catch(err => console.error(err));
 
