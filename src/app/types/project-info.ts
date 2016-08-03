2df97d4e1e5059c37baf78c44bd4a5c1934afd8c
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2016-08-03 11:41:32 +0200

    added type UploadInfo

--- src/app/types/project-info.ts
+++ src/app/types/project-info.ts
@@ -1,6 +1,7 @@
 import {DatabaseInfo} from "./database-info";
+import {UploadInfo} from "./upload-info";
 export interface ProjectInfo {
 	name:string;
 	databases:DatabaseInfo[];
-	uploads;
+	uploads:UploadInfo[];
 }
---
+++ src/app/types/upload-info.ts
@@ -0,0 +1,6 @@
+import {SessionInfo} from "./session-info";
+export interface UploadInfo {
+	uuid:string;
+	date:string;
+	sessions:SessionInfo[];
+}
