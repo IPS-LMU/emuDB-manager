2ca54fa19921fc4087d393cefbd188597b952f20
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2016-08-08 15:38:15 +0200

    json helper functions now return HelperResult objects rather than boolean

--- src/server-side/json_file.php
+++ src/server-side/json_file.php
@@ -6,28 +6,39 @@
 // However, it is no security issue if it is called directly, because it only
 // contains functions (thus, no code is executed).
 
+require_once 'type_definitions.php';
+require_once 'helper_result.php';
+
 /**
  * Opens a file, decodes the JSON contained in it and returns the decoded
  * object.
  *
  * @params $filename The JSON file to decode.
- * @returns The object stored in $filename in case of success.
- * @returns false In case of failure.
+ * @returns A HelperResult object.
  */
 function load_json_file ($filename) {
 	$length = filesize ($filename);
 	if ($length === false) {
-		return false;
+		return helperFailure (
+			'LOAD_JSON_FAILED',
+			'Loading a JSON file failed'
+		);
 	}
 
 	$fh = fopen($filename, 'r');
 	if ($fh === false) {
-		return false;
+		return helperFailure (
+			'LOAD_JSON_FAILED',
+			'Loading a JSON file failed'
+		);
 	}
 
 	$contents = fread($fh, $length);
 	if ($contents === false) {
-		return false;
+		return helperFailure (
+			'LOAD_JSON_FAILED',
+			'Loading a JSON file failed'
+		);
 	}
 
 	fclose ($fh);
@@ -35,10 +46,13 @@ function load_json_file ($filename) {
 
 	$object = json_decode($contents);
 	if (is_null($object)) {
-		return false;
+		return helperFailure (
+			'PARSE_JSON_FAILED',
+			'Loading a JSON file failed'
+		);
 	}
 
-	return $object;
+	return helperSuccess($object);
 }
 
 /**
@@ -46,28 +60,37 @@ function load_json_file ($filename) {
  *
  * @param $object The object to encode.
  * @param $filename The JSON file to write to.
- * @returns True in case of success, false in case of failure.
+ * @returns A HelperResult object.
  */
 function save_json_file ($object, $filename) {
 	$json = json_encode($object);
 
 	if ($json === false) {
-		return false;
+		return helperFailure (
+			'SAVE_JSON_FAILED',
+			'Saving a JSON file failed'
+		);
 	}
 
 	$fh = fopen ($filename, 'w');
 	if ($fh === false) {
-		return false;
+		return helperFailure (
+			'SAVE_JSON_FAILED',
+			'Saving a JSON file failed'
+		);
 	}
 
 	if (fwrite ($fh, $json) === false) {
-		return false;
+		return helperFailure (
+			'SAVE_JSON_FAILED',
+			'Saving a JSON file failed'
+		);
 	}
 
 	fclose($fh);
 	// fclose errors are ignored
 
-	return true;
+	return helperSuccess(null);
 }
 
 ?>
