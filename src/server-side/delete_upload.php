238bd94fcbed6a4d0bc647412a6d419a89429e4d
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2016-08-17 11:43:33 +0200

    finished server-side function to delete an upload

--- src/server-side/delete_upload.php
+++ src/server-side/delete_upload.php
@@ -17,5 +17,66 @@ require_once 'result_helper.php';
  * @return Result
  */
 function delete_upload ($projectDir, $uuid) {
+	$uploadDir = $projectDir . '/uploads/' . $uuid;
+
+	// Recursively delete $uploadDir
+	// But *DO NOT* follow any symlinks - this is vital!
+	//
+	// If we did, people might upload symlinks and we would happily delete
+	// any file the web server has access to.
+
+	try {
+		$files = new RecursiveIteratorIterator(
+			new RecursiveDirectoryIterator(
+				$uploadDir,
+				RecursiveDirectoryIterator::SKIP_DOTS
+			// no FOLLOW_SYMLINKS flag here!!
+			),
+			RecursiveIteratorIterator::CHILD_FIRST
+		);
+	} catch (Exception $e) {
+		return negativeResult(
+			'INVALID_UPLOAD_DIR',
+			'The upload directory could not be accessed.'
+		);
+	}
+
+	foreach ($files as $fileInfo) {
+		// *do not* use $fileInfo->getRealPath() - this would follow symlinks
+		$name = $fileInfo->getPath() . '/' . $fileInfo->getFilename();
+
+		// isLink and isDir *both* return true for symlinks to directories
+		// isLink must therefore be checked first
+		if ($fileInfo->isLink()) {
+			if (!unlink($name)) {
+				return negativeResult(
+					'DELETE_INCOMPLETE',
+					'The upload directory was not deleted completely.'
+				);
+			}
+		} else if ($fileInfo->isDir()) {
+			if (!rmdir ($name)) {
+				return negativeResult(
+					'DELETE_INCOMPLETE',
+					'The upload directory was not deleted completely.'
+				);
+			}
+		} else {
+			if (!unlink($name)) {
+				return negativeResult(
+					'DELETE_INCOMPLETE',
+					'The upload directory was not deleted completely.'
+				);
+			}
+		}
+	}
+
+	if (!rmdir($uploadDir)) {
+		return negativeResult(
+			'DELETE_INCOMPLETE',
+			'The upload directory was not deleted completely.'
+		);
+	}
+
 	return positiveResult(null);
 }
