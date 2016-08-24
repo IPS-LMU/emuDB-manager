f0e89a20dfb54fb18baa73dceacae782d3a9436f
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2016-08-24 14:53:40 +0200

    ng2-uploader bundled its own copy of angular in build

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -16,7 +16,8 @@ module.exports = function(defaults) {
       'reflect-metadata/**/*.+(ts|js|js.map)',
       'rxjs/**/*.+(js|js.map)',
       '@angular/**/*.+(js|js.map)',
-	  'ng2-uploader/**/*.+(js|js.map)',
+      'ng2-uploader/*.+(js|js.map)',
+      'ng2-uploader/src/**/*.+(js|js.map)',
       'jquery/dist/*'
     ]
   });
