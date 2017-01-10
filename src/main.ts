1deaa3407af3fe75abf8ab6e3467059e758663a3
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2017-01-10 23:22:50 +0100

    angular-cli update beta.10->beta.24: changed infrastructure files

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import {enableProdMode} from "@angular/core";
-import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
-import {AppModule} from "./app/app.module";
-import {environment} from "./app/";
+import './polyfills.ts';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+import { AppModule } from './app/app.module';
 
 if (environment.production) {
 	enableProdMode();
