2d3f38e7a5f5dda2ace8ab315a7aa96b6ef2d59e
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2017-01-18 14:53:55 +0100

    merge_upload: finished up to and including the config comparison

--- src/server-side/queryHandlers/merge_upload.php
+++ src/server-side/queryHandlers/merge_upload.php
@@ -8,10 +8,14 @@
 
 require_once 'helpers/filenames.php';
 require_once 'helpers/git.php';
+require_once 'helpers/json_file.php';
 require_once 'helpers/result_helper.php';
 
 /**
  * Merge an upload into an existing database.
+ *
+ * Before this can be done, the configuration of the two databases have two
+ * be compared.
  *
  * @param $projectDir string The project directory for which the client has
  *        been authorized.
@@ -22,16 +26,117 @@ require_once 'helpers/result_helper.php';
  * @return Result
  */
 function merge_upload ($projectDir, $uploadUUID, $targetDB) {
-	$targetDBDir = getDatabaseDirectory($projectDir, $targetDB);
 
-	$uploadDir = getUploadDirectory($projectDir, $uploadUUID);
-	$result = findDatabaseInUpload($uploadDir);
+	///////////////
+	// Find the required file names
+	//
+	$targetDBConfigFile = getDatabaseConfigFile($projectDir, $targetDB);
+
+	// Find config file in upload
+	$uploadDirectory = getUploadDirectory($projectDir, $uploadUUID);
+
+	$result = findDatabaseInUpload($uploadDirectory);
 	if ($result->success !== true) {
 		return $result;
 	}
-	$uploadedDBDir = $uploadDir . '/data/' . $result->data . '_emuDB';
+	$uploadDBName = $result->data;
 
-	// @todo do the actual thing
+	$uploadDBConfigFile = getUploadDatabaseConfigFile(
+		$projectDir, $uploadUUID, $uploadDBName
+	);
+
+	//
+	///////////////
+
+
+	///////////////
+	// Load the configuration files
+	//
+	$result = load_json_file($targetDBConfigFile);
+	if ($result->success !== true) {
+		return negativeResult(
+			'TARGET_DB_CONFIG_NOT_READABLE',
+			'Could not read target database’s config file.'
+		);
+	}
+	$targetConfig = $result->data;
+
+	$result = load_json_file($uploadDBConfigFile);
+	if ($result->success !== true) {
+		return negativeResult(
+			'UPLOAD_DB_CONFIG_NOT_READABLE',
+			'Could not read uploaded database’s config file.'
+		);
+	}
+	$uploadConfig = $result->data;
+
+	//
+	///////////////
+
+
+	///////////////
+	// Compare configurations
+	//
+
+	// With the == operator, objects can be compared recursively.
+	// See http://php.net/manual/en/language.oop5.object-comparison.php
+
+	// ssffTrackDefinitions is an array of objects
+	// != compares it recursively
+	if ($targetConfig->ssffTrackDefinitions !=
+		$uploadConfig->ssffTrackDefinitions
+	) {
+		return negativeResult(
+			'CONFIGURATION_MISMATCH',
+			'The configuration of the uploaded database differs from the ' .
+			'target database’s (ssffTrackDefinitions)'
+		);
+	}
+
+	// levelDefinitions is an array of objects
+	// != compares it recursively
+	if ($targetConfig->levelDefinitions !=
+		$uploadConfig->levelDefinitions
+	) {
+		return negativeResult(
+			'CONFIGURATION_MISMATCH',
+			'The configuration of the uploaded database differs from the ' .
+			'target database’s (levelDefinitions)'
+		);
+	}
+
+	// linkDefinitions is an array of objects
+	// != compares it recursively
+	if ($targetConfig->linkDefinitions !=
+		$uploadConfig->linkDefinitions
+	) {
+		return negativeResult(
+			'CONFIGURATION_MISMATCH',
+			'The configuration of the uploaded database differs from the ' .
+			'target database’s (linkDefinitions)'
+		);
+	}
+
+	// mediaFileExtension is supposed to be a string and can be compared with
+	// !==
+	if ($targetConfig->mediaFileExtension !== $uploadConfig->mediaFileExtension) {
+		return negativeResult(
+			'CONFIGURATION_MISMATCH',
+			'The configuration of the uploaded database differs from the ' .
+			'target database’s (mediaFileExtension)'
+		);
+	}
+
+	//
+	///////////////
+
+
+	///////////////
+	// Copy new sessions and bundles over to the existing database
+	//
+
+	//
+	///////////////
 
 	return positiveResult(null);
 }
